cb8c11c64521bef0d6e9b5c084fd532d95338ce9
kdani95 <kubischdani@gmail.com>
2018-05-20 21:08:13 +0200

    Trying to resolve dependecy bugs

--- ProjEszk/Pancake/src/pancake_server/Pancake_server.java
+++ ProjEszk/Pancake/src/pancake_server/Pancake_server.java
@@ -1,8 +1,22 @@
 package pancake_server;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class Pancake_server {
 
     public static void main(String[] args) {
+        
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+        
+        
+        
         Server.ServerStarter.main(args);
     }
     
